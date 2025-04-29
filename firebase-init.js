685f93181359eb0a6ccd2e9eaf74b85948bb29f8
Eduardo Lipoli <eduardolipoli95@gmail.com>
2025-04-29 13:43:17 -0400

    modified: itens

--- firebase-init.js
+++ firebase-init.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const firebaseConfig = {
   apiKey: "AIzaSyB_ya2Ixz4GhtBbTIauiPJcHtzyob1CF6Y",
   authDomain: "lista-compras-37af3.firebaseapp.com",
@@ -9,15 +8,3 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-=======
-const firebaseConfig = {
-  apiKey: "AIzaSyB_ya2Ixz4GhtBbTIauiPJcHtzyob1CF6Y",
-  authDomain: "lista-compras-37af3.firebaseapp.com",
-  projectId: "lista-compras-37af3",
-  storageBucket: "lista-compras-37af3.firebasestorage.app",
-  messagingSenderId: "493934383937",
-  appId: "1:493934383937:web:53e885bdda84cb04b647c0"
-};
-
-firebase.initializeApp(firebaseConfig);
->>>>>>> c5e4f847c668822bf5b319a09e1222e431a0721b
--- login.js
+++ login.js
@@ -1,36 +1,17 @@
-<<<<<<< HEAD
-    const auth = firebase.auth();
+const auth = firebase.auth();
 
-    // Fazer login
-    document.getElementById('loginForm').addEventListener('submit', e => {
-      e.preventDefault();
-      const email = document.getElementById('email').value;
-      const senha = document.getElementById('senha').value;
-      auth.signInWithEmailAndPassword(email, senha)
-        .catch(err => alert('Falha no login: ' + err.message));
-    });
+// Fazer login
+document.getElementById('loginForm').addEventListener('submit', e => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+  auth.signInWithEmailAndPassword(email, senha)
+    .catch(err => alert('Falha no login: ' + err.message));
+});
 
-    // Se estiver logado, redireciona para app principal
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        window.location.href = 'index.html';
-      }
-=======
-    const auth = firebase.auth();
-
-    // Fazer login
-    document.getElementById('loginForm').addEventListener('submit', e => {
-      e.preventDefault();
-      const email = document.getElementById('email').value;
-      const senha = document.getElementById('senha').value;
-      auth.signInWithEmailAndPassword(email, senha)
-        .catch(err => alert('Falha no login: ' + err.message));
-    });
-
-    // Se estiver logado, redireciona para app principal
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        window.location.href = 'index.html';
-      }
->>>>>>> c5e4f847c668822bf5b319a09e1222e431a0721b
-    });
+// Se estiver logado, redireciona para app principal
+auth.onAuthStateChanged(user => {
+  if (user) {
+    window.location.href = 'index.html';
+  }
+});

0c5e7febeec5c54f7474a528f7d55e55e9d09a43
Eduardo Lipoli <eduardolipoli95@gmail.com>
2025-04-29 13:49:21 -0400

    ajustes: erros de merge

--- loading.js
+++ loading.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function showLoading() {
     const loadingDiv = document.createElement("div");
     loadingDiv.classList.add("loading");
@@ -16,22 +15,3 @@ function hideLoading() {
         loadingDiv.remove();
     }
 }
-=======
-function showLoading() {
-    const loadingDiv = document.createElement("div");
-    loadingDiv.classList.add("loading");
-
-    const loader = document.createElement("div");
-    loader.classList.add("loader-circle");
-
-    loadingDiv.appendChild(loader);
-    document.body.appendChild(loadingDiv);
-}
-
-function hideLoading() {
-    const loadingDiv = document.querySelector(".loading");
-    if (loadingDiv) {
-        loadingDiv.remove();
-    }
-}
->>>>>>> c5e4f847c668822bf5b319a09e1222e431a0721b
